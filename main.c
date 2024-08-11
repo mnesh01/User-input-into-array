7f1a8f7e5655d176b38f2dbc4f066484320a1605
mnesh01 <97187030+mnesh01@users.noreply.github.com>
2024-08-11 13:04:11 +0300

    insert new element into a specific position

--- main.c
+++ main.c
@@ -1,24 +1,46 @@
-// user input into an array
+//P15//139900/2020
+//ASSIGNMENT 2
+
 #include <stdio.h>
 int main()
 {
-    int n;
-    int j=0;
+  int Array[30];
+  int pos;
+  int i;
+  int n;
+  int value;
+  printf("Enter the number of elements in an array\n");
+  scanf("%d", &n);
+ 
+  printf("\nEnter %d elements of an array \n", n);
+  for (i=0;i<n;i++)
+   {
+     scanf("%d", &Array[i]);
+   }
     
-    printf("Enter the number of integers you want to enter:\n");
-    scanf("%d",&n);
-    if(n<=0)
-    {
-        printf("Invalid input!\n");
-    }
+  printf("\nEnter the position of the element you want to insert in the array\n");
+  scanf("%d", &pos);
+    
+  if(pos>=1 && pos<=n+1)
+  {
+  printf("\nEnter the value of the element you want to insert in the array\n");
+  scanf("%d", &value);
+  for (i=n-1;i>=pos-1;i--)
+   {
+         Array[i+1] = Array[i];
+   }
+  Array[pos-1] = value;
+      
+ printf("\nThe final Array after Inserting an the new element is:\n");
+ for (i=0;i<=n;i++)
+  {
+     printf("%d\t", Array[i]);
+  }
+  }
+    
     else
     {
-    int a[n];
-    printf("Enter the integers:\n");
-    for(j=0;j<n;j++)
-    {
-        scanf("%d",&a[j]);
+        printf("\nINVALID POSITION\n");
     }
-    }
+ return 0;
 }
- 
